5bc4a4b91a201fa79b57b821ed2e905552c9f948
LittleStupid <lttlstpd@live.com>
2016-07-29 08:45:04 +0800

    config

--- parser.rb
+++ parser.rb
@@ -8,14 +8,14 @@ PIXIV = "http://www.pixiv.net/"
 BAIDU = "https://www.baidu.com"
 ADDRESS = "https://h.nimingban.com/Forum"
 MAX_DEPTH = 90
-URL_MAX_LENGTH = 60
+URL_MAX_LENGTH = 40
 IMG_REQUIRE_SIZE_IN_BYTE = 1024 * 384
 
 URLS = []
 $count = 0
 $visited_urls = {}
 
-BAN_WORDS = [ "163", "qq", "gov", "acg", "123", "72g", "1688", "18", "dnf", "7" ]
+BAN_WORDS = [ "163", "qq", "gov", "acg", "123", "72g", "1688", "18", "dnf", "7", "youku" ]
 
 def valid_url?( url )
   url ||= ""
